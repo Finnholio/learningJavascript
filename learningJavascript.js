f5f4193edd55a7cdbe5a0e095e524a732cb9b3d3
Finnholio <50093136+Finnholio@users.noreply.github.com>
2022-02-16 15:47:07 -0600

    Create learning javascript.js

---
+++ learningJavascript.js
@@ -0,0 +1,135 @@
+// Try other templates: Project -> New
+let amount = Math.random(0 ,1)
+
+let newMessage = "get me out"
+
+console.log(newMessage.length)
+
+let lateToSchool = true
+
+let pi = 3.14
+console.log(Math.floor(pi))
+
+let x = null
+
+let single = 'hellowo'
+let double = "kind fun owo"
+
+5 + 5
+20 - 10
+2 * 5
+30 / 3
+
+/*hi! 
+i'm a multi line comment
+thats fun!
+*/
+
+20 % 4
+
+pi += 1
+pi -= 1
+pi *= 1
+
+let age = 5
+
+console.log("Timmy is " + age + " and we hate him for it")
+
+const death = "death is always constant"
+
+var a
+console.log(a)
+
+console.log('Timmy is still ${age} idk why this does not work')
+
+let me = ">:"
+console.log(me)
+
+me = "<:"
+console.log(me)
+
+age === "5" ? age += 1 : age = death
+
+if (age == death && pi != 3.14) {
+  console.log("nothing makes sense anymore")
+} 
+else {
+  console.log("I guess we're good")
+}
+
+food = "crab"
+
+switch (food) {
+  case 'oaster':
+    console.log("to the moon and back")
+    break
+  case 'pizza':
+    console.log("ahahaha")
+  default:
+    console.log("no one cares about your food")
+}
+
+if (food != "crab"){
+  console.log("Its definitly not a crab")
+}
+
+const sum = (thing1, thing2) => {return thing1 + thing2}
+
+console.log(sum(1, 2))
+
+function mult(thing1, thing2) {
+  return thing1 * thing2
+}
+
+console.log(mult(2, 3))
+
+const aCoolFunction = function(coolStuff) {
+  console.log(coolStuff)
+}
+
+aCoolFunction("AHAAHAHAAHAHAAH")
+
+const numbers = [0, 1, 2, 3, 4]
+
+console.log(numbers.length)
+
+console.log(numbers[0])
+
+numbers.push("hello")
+
+console.log(numbers)
+
+numbers.pop()
+
+console.log(numbers)
+
+let i = 5
+while (i > 0) {
+  console.log(i)
+  i -= 1
+}
+
+fruit = ["bannana", "apple", "cherry"]
+console.log(fruit)
+
+for (let k = fruit.length - 1; k >= 0; k -= 1) {
+  console.log(fruit[k])
+}
+
+p = 0
+do {
+  console.log(fruit[p])
+  p += 1
+
+} while(p < fruit.length)
+
+for (let i = 0; i < 3; i += 1) {
+  for (let y = 0; y < 3; y += 1) {
+    console.log(i + ", " + y)
+  }
+}
+
+
+
+
+
